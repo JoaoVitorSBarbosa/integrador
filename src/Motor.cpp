fba9f515cb5cc83e76d1aafa6e58e35b99259583
Paulo-Junio <paulofernandesbraga@gmail.com>
2026-05-14 18:25:37 -0300

    feat: reescreve Motor para BTS7960 com LEDC

--- src/Motor.cpp
+++ src/Motor.cpp
@@ -1,19 +1,31 @@
 #include "Motor.h"
 
-Motor::Motor(uint8_t pwm, uint8_t dir)
-    : pinPWM(pwm), pinDIR(dir) {}
+Motor::Motor(uint8_t rpwm, uint8_t lpwm, uint8_t ledcChRpwm, uint8_t ledcChLpwm)
+    : pinRPWM(rpwm), pinLPWM(lpwm), chRPWM(ledcChRpwm), chLPWM(ledcChLpwm) {}
 
 void Motor::begin() {
-    pinMode(pinDIR, OUTPUT);
-    pinMode(pinPWM, OUTPUT);
-    digitalWrite(pinDIR, LOW);
-    analogWrite(pinPWM, 0);
+    ledcSetup(chRPWM, MOTOR_PWM_FREQ_HZ, MOTOR_PWM_RESOLUTION);
+    ledcSetup(chLPWM, MOTOR_PWM_FREQ_HZ, MOTOR_PWM_RESOLUTION);
+    ledcAttachPin(pinRPWM, chRPWM);
+    ledcAttachPin(pinLPWM, chLPWM);
+    parar();
 }
 
 void Motor::setVelocidade(float sinalControle) {
-    int duty = (int)constrain(abs(sinalControle), 0.0f, 255.0f);
-    digitalWrite(pinDIR, sinalControle >= 0 ? HIGH : LOW);
-    // TODO: substituir por ledcWrite quando hardware disponivel
-    Serial.printf("[Motor] dir=%s duty=%d\n",
-        sinalControle >= 0 ? "FWD" : "REV", duty);
+    int duty = static_cast<int>(
+        constrain(abs(sinalControle), 0.0f, static_cast<float>(MOTOR_PWM_MAX_DUTY)));
+    if (sinalControle > 0.0f) {
+        ledcWrite(chRPWM, duty);
+        ledcWrite(chLPWM, 0);
+    } else if (sinalControle < 0.0f) {
+        ledcWrite(chRPWM, 0);
+        ledcWrite(chLPWM, duty);
+    } else {
+        parar();
+    }
+}
+
+void Motor::parar() {
+    ledcWrite(chRPWM, 0);
+    ledcWrite(chLPWM, 0);
 }
